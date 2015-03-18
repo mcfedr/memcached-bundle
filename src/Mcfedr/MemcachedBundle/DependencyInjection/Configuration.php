0be64da8a4862b4eac7be0b63017647baf64f02c
Fred Cox <mcfedr@gmail.com>
2015-03-18 23:51:46 +0200

    Remove unused

--- src/Mcfedr/MemcachedBundle/DependencyInjection/Configuration.php
+++ src/Mcfedr/MemcachedBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mcfedr_memcached')
+        $treeBuilder->root('mcfedr_memcached')
             ->children()
                 ->arrayNode('connections')
                     ->requiresAtLeastOneElement()
